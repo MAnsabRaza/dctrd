7ca7b66a1b4b33e099fc650029ac6dd2839a402e
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-05-21 23:23:02 +0500

    correct migration

--- database/migrations/2026_05_21_000000_add_booking_id_to_cart_table.php
+++ database/migrations/2026_05_21_000000_add_booking_id_to_cart_table.php
@@ -6,31 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class AddBookingIdToCartTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('cart', function (Blueprint $table) {
-            $table->integer('booking_id')->unsigned()->nullable()->after('webinar_id');
-            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+
+            $table->unsignedBigInteger('booking_id')
+                ->nullable()
+                ->after('webinar_id');
+
+            $table->foreign('booking_id')
+                ->references('id')
+                ->on('bookings')
+                ->onDelete('cascade');
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('cart', function (Blueprint $table) {
-            if (Schema::hasColumn('cart', 'booking_id')) {
-                $table->dropForeign(['booking_id']);
-                $table->dropColumn('booking_id');
-            }
+
+            $table->dropForeign(['booking_id']);
+
+            $table->dropColumn('booking_id');
+
         });
     }
 }
