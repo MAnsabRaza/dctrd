817c7af3cc0685ca00dea0ef7964e4f895c656a9
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-06-20 19:32:45 +0500

    add column in comment and comment report

---
+++ database/migrations/2026_06_20_142800_add_booking_id_to_comments_reports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments_reports', function (Blueprint $table) {
+             $table->unsignedBigInteger('booking_id')
+                  ->nullable()
+                  ->after('product_id');
+
+            $table->foreign('booking_id')
+                  ->references('id')
+                  ->on('bookings')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('comments_reports', function (Blueprint $table) {
+            $table->dropForeign(['booking_id']);
+            $table->dropColumn('booking_id');
+        });
+    }
+};
---
+++ database/migrations/2026_06_20_143104_add_booking_fields_to_comments_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+             $table->unsignedBigInteger('booking_id')
+                  ->nullable()
+                  ->after('product_id');
+
+            $table->unsignedBigInteger('booking_review_id')
+                  ->nullable()
+                  ->after('product_review_id');
+
+            // Foreign Keys
+            $table->foreign('booking_id')
+                  ->references('id')
+                  ->on('bookings')
+                  ->onDelete('cascade');
+
+            $table->foreign('booking_review_id')
+                  ->references('id')
+                  ->on('booking_reviews')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign(['booking_id']);
+            $table->dropForeign(['booking_review_id']);
+
+            $table->dropColumn(['booking_id', 'booking_review_id']);
+        });
+    }
+};
