7b4150ebae1867baf2f148fdb7227ac0b07d2fa4
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-06-25 10:15:40 +0500

    correct orgainzaton booking icon

--- resources/views/design_1/panel/bookings/create_booking/index.blade.php
+++ resources/views/design_1/panel/bookings/create_booking/index.blade.php
@@ -9,6 +9,7 @@
 @extends('design_1.panel.layouts.panel')
 
 @section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <style>
 /* ============================================================
