c1b950d25a43bb4791851f4789333cda039aa369
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-07-25 17:44:12 +0500

    correct booking panel

--- routes/panel.php
+++ routes/panel.php
@@ -115,11 +115,12 @@ Route::group(['namespace' => 'Panel', 'prefix' => 'panel', 'middleware' => ['imp
 
         Route::delete('/time-slots/{timeSlot}', 'Booking\BookingController@destroyTimeSlot')
             ->name('panel.bookings.time_slots.destroy');
-                Route::post('/{id}/availability-overrides', 'Booking\BookingController@storeAvailability')
-            ->name('panel.bookings.availability_overrides.store');
 
-        Route::delete('/availability-overrides/{availability}', 'Booking\BookingController@destroyAvailability')
-            ->name('panel.bookings.availability_overrides.destroy');
+       Route::post('/{id}/availability-overrides', 'Booking\BookingController@storeAvailabilityOverride')
+    ->name('panel.bookings.availability_overrides.store');
+
+Route::delete('/availability-overrides/{availability}', 'Booking\BookingController@destroyAvailabilityOverride')
+    ->name('panel.bookings.availability_overrides.destroy');
 
         Route::post('/{id}/faqs', 'Booking\BookingController@storeFaq')
             ->name('panel.bookings.faqs.store');
