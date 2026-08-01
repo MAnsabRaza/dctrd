865c496001002ced7f2aaa62d3efed5e84044bc0
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-08-01 15:25:04 +0500

    correct role management

--- resources/views/design_1/panel/roles/index.blade.php
+++ resources/views/design_1/panel/roles/index.blade.php
@@ -3,10 +3,7 @@
 @section('content')
 <div class="section-header d-flex justify-content-between align-items-center mb-16">
     <h1 class="font-20 font-weight-bold">{{ trans('panel.my_roles') }}</h1>
-    <div class="section-header-breadcrumb">
-        <a href="{{ url('/panel') }}">{{ trans('panel.dashboard') }}</a> /
-        <span>{{ trans('panel.my_roles') }}</span>
-    </div>
+ 
 </div>
 
 <div class="bg-white p-16 rounded-16 border-gray-200">
