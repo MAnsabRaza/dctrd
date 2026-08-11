c8af306a9c05ec1d366925ed14be76d87001a613
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-08-11 21:05:10 +0500

    correct product

--- resources/views/design_1/web/products/show/index.blade.php
+++ resources/views/design_1/web/products/show/index.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white p-16 rounded-24 mt-24">
+<!-- <div class="bg-white p-16 rounded-24 mt-24">
     <h3 class="font-16">{{ trans('update.product_specifications') }}</h3>
 
     <div class="">
@@ -24,4 +24,100 @@
             @endforeach
         @endif
     </div>
-</div>
+</div> -->
+
+@extends('design_1.web.layouts.app')
+
+@push('styles_top')
+    <link rel="stylesheet" href="{{ getDesign1StylePath("product_show") }}">
+@endpush
+
+@section('content')
+    <div class="container mt-160 mb-160">
+
+        {{-- Advertising Banners --}}
+        @if(!empty($advertisingBanners) and count($advertisingBanners))
+            <div class="mb-16">
+                @foreach($advertisingBanners as $advertisingBanner)
+                    <a href="{{ $advertisingBanner->url }}" target="_blank">
+                        <img src="{{ $advertisingBanner->image }}" alt="{{ $advertisingBanner->title }}" class="img-fluid rounded-24">
+                    </a>
+                @endforeach
+            </div>
+        @endif
+
+        <div class="row">
+
+            {{-- Images --}}
+            <div class="col-12 col-lg-5">
+                @include('design_1.web.products.show.includes.images', ['product' => $product])
+            </div>
+
+            {{-- Main Info (title, price, quantity, add to cart, buy with points, installments) --}}
+            <div class="col-12 col-lg-7 mt-24 mt-lg-0">
+                @include('design_1.web.products.show.includes.main_info', [
+                    'product' => $product,
+                    'user' => $user,
+                    'selectableSpecifications' => $selectableSpecifications,
+                    'hasInstallments' => $hasInstallments,
+                ])
+            </div>
+
+        </div>
+
+        {{-- Promotions (gift card / cashback / instructor discounts) --}}
+        @include('design_1.web.products.show.includes.promotions', [
+            'product' => $product,
+            'productAvailability' => $productAvailability,
+            'cashbackRules' => $cashbackRules,
+            'instructorDiscounts' => $instructorDiscounts,
+        ])
+
+        {{-- About / Description / FAQ --}}
+        @include('design_1.web.products.show.includes.about', ['product' => $product])
+
+        {{-- Specifications --}}
+        @include('design_1.web.products.show.includes.specifications.index', [
+            'selectedSpecifications' => $selectedSpecifications,
+        ])
+
+        {{-- Files (only for products that have downloadable files) --}}
+        @if(!empty($product->files) and count($product->files))
+            <div class="mt-24">
+                @include('design_1.web.products.show.includes.files', ['product' => $product])
+            </div>
+        @endif
+
+        {{-- Seller / Instructor Card --}}
+        @include('design_1.web.products.show.includes.seller', [
+            'product' => $product,
+            'seller' => $seller,
+            'sellerBadges' => $sellerBadges,
+            'sellerRates' => $sellerRates,
+            'sellerFollowers' => $sellerFollowers,
+            'sellerFollowing' => $sellerFollowing,
+            'authUserIsFollower' => $authUserIsFollower,
+        ])
+
+        {{-- Reviews --}}
+        <div class="mt-24">
+            @include('design_1.web.products.show.includes.reviews', [
+                'product' => $product,
+                'productReviews' => $productReviews,
+            ])
+        </div>
+
+        {{-- Comments --}}
+        <div class="mt-24">
+            @include('design_1.web.products.show.includes.comments', [
+                'product' => $product,
+                'productComments' => $productComments,
+            ])
+        </div>
+
+    </div>
+@endsection
+
+@push('scripts_bottom')
+    <script src="{{ getDesign1ScriptPath("product_show") }}"></script>
+@endpush
