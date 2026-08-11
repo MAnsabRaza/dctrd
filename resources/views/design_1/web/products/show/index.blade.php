209849d5e31845086db9a977ac6048800bc6e91a
MAnsabRaza <email.ansabnasir153@gmail.com>
2026-08-11 21:14:26 +0500

    correct product index

--- resources/views/design_1/web/products/show/index.blade.php
+++ resources/views/design_1/web/products/show/index.blade.php
@@ -1,31 +1,3 @@
-<!-- <div class="bg-white p-16 rounded-24 mt-24">
-    <h3 class="font-16">{{ trans('update.product_specifications') }}</h3>
-
-    <div class="">
-        @if(!empty($selectedSpecifications) and count($selectedSpecifications))
-            @foreach($selectedSpecifications as $selectedSpecification)
-                <div class="product-show__specification-item d-flex align-items-start mt-16 {{ (!$loop->first) ? 'pt-16 border-top-gray-200' : '' }} ">
-                    <div class="specification-item-name font-weight-bold text-gray-500 ">
-                        {{ $selectedSpecification->specification->title }}
-                    </div>
-
-                    <div class="specification-item-value flex-grow-1 px-16">
-                        @if($selectedSpecification->type == 'textarea')
-                            {!! nl2br($selectedSpecification->value) !!}
-                        @elseif(!empty($selectedSpecification->selectedMultiValues))
-                            @foreach($selectedSpecification->selectedMultiValues as $selectedSpecificationValue)
-                                @if(!empty($selectedSpecificationValue->multiValue))
-                                    <span class="d-block">{{ $selectedSpecificationValue->multiValue->title }}</span>
-                                @endif
-                            @endforeach
-                        @endif
-                    </div>
-                </div>
-            @endforeach
-        @endif
-    </div>
-</div> -->
-
 @extends('design_1.web.layouts.app')
 
 @push('styles_top')
@@ -50,12 +22,12 @@
 
             {{-- Images --}}
             <div class="col-12 col-lg-5">
-                @include('design_1.web.products.show.includes.images', ['product' => $product])
+                @include('design_1.web.products.show.includes.main_info.images', ['product' => $product])
             </div>
 
             {{-- Main Info (title, price, quantity, add to cart, buy with points, installments) --}}
             <div class="col-12 col-lg-7 mt-24 mt-lg-0">
-                @include('design_1.web.products.show.includes.main_info', [
+                @include('design_1.web.products.show.includes.main_info.main_info', [
                     'product' => $product,
                     'user' => $user,
                     'selectableSpecifications' => $selectableSpecifications,
@@ -66,7 +38,7 @@
         </div>
 
         {{-- Promotions (gift card / cashback / instructor discounts) --}}
-        @include('design_1.web.products.show.includes.promotions', [
+        @include('design_1.web.products.show.includes.main_info.promotions', [
             'product' => $product,
             'productAvailability' => $productAvailability,
             'cashbackRules' => $cashbackRules,
@@ -74,22 +46,22 @@
         ])
 
         {{-- About / Description / FAQ --}}
-        @include('design_1.web.products.show.includes.about', ['product' => $product])
+        @include('design_1.web.products.show.includes.main_info.tabs.about', ['product' => $product])
 
         {{-- Specifications --}}
-        @include('design_1.web.products.show.includes.specifications.index', [
+        @include('design_1.web.products.show.includes.main_info.tabs.specifications', [
             'selectedSpecifications' => $selectedSpecifications,
         ])
 
         {{-- Files (only for products that have downloadable files) --}}
         @if(!empty($product->files) and count($product->files))
             <div class="mt-24">
-                @include('design_1.web.products.show.includes.files', ['product' => $product])
+                @include('design_1.web.products.show.includes.main_info.tabs.files', ['product' => $product])
             </div>
         @endif
 
         {{-- Seller / Instructor Card --}}
-        @include('design_1.web.products.show.includes.seller', [
+        @include('design_1.web.products.show.includes.main_info.tabs.seller', [
             'product' => $product,
             'seller' => $seller,
             'sellerBadges' => $sellerBadges,
@@ -101,7 +73,7 @@
 
         {{-- Reviews --}}
         <div class="mt-24">
-            @include('design_1.web.products.show.includes.reviews', [
+            @include('design_1.web.products.show.includes.main_info.tabs.reviews', [
                 'product' => $product,
                 'productReviews' => $productReviews,
             ])
@@ -109,7 +81,7 @@
 
         {{-- Comments --}}
         <div class="mt-24">
-            @include('design_1.web.products.show.includes.comments', [
+            @include('design_1.web.products.show.includes.main_info.tabs.comments', [
                 'product' => $product,
                 'productComments' => $productComments,
             ])
@@ -117,7 +89,3 @@
 
     </div>
 @endsection
-
-@push('scripts_bottom')
-    <script src="{{ getDesign1ScriptPath("product_show") }}"></script>
-@endpush
